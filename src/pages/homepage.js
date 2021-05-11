55e77a8e8d7ee409651bd5ee943b168b2d34fc1f
atr11_Art Treleaven <artt@shaw.ca>
2021-05-11 16:50:03 -0600

    Modified HOME Page to test GET method

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-const HomePage = () => (
-    <div className='homepage'>
-        HOMEPAGE
-    </div>
-)
+const HomePage = () => {
+    const [user, setUser] = useState([])
+
+    useEffect(() => {
+        const getUser = async () => {
+            let response = await fetch('/users/60917ac1f7ccfb4de4eb5d31')
+            let data = await response.json()
+            setUser(data)
+            console.log(data)
+        }
+        getUser()
+        console.log({user})
+    }, [])
+
+    // useEffect(() => {
+    //     // POST request using fetch inside useEffect React hook
+    //     const addUserOptions = {
+    //         method: 'POST',
+    //         headers: { 'Content-Type': 'application/json' },
+    //         body: JSON.stringify({ title: 'React Hooks POST Request Example' })
+    //     };
+    //     fetch('/users', addUserOptions)
+    //         .then(response => response.json())
+    //         .then(data => setPostId(data.id));
+    
+    // // empty dependency array means this effect will only run once (like componentDidMount in classes)
+    // }, []);
+
+    return (
+        <div className='homepage'>
+            {user.firstName}
+        </div>
+    )
+}
 
 export default HomePage
